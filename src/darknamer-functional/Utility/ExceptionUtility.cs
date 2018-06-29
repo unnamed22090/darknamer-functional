955c5b4c3f712eeb4b9a2a6ba628759642e4189c
pongpl49 <pongpl49@aware.postbox.in.th>
2018-06-29 14:34:07 +0700

    Init project dotnet

---
+++ src/darknamer-functional/Utility/ExceptionUtility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public class ExceptionUtility
+    {
+        /// <summary>
+        /// Functional for get inner of exception and return inner exception.
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public static Exception GetInnerException(Exception exception)
+        {
+            while (true)
+            {
+                if (exception.InnerException != null)
+                {
+                    exception = exception.InnerException;
+                    continue;
+                }
+                break;
+            }
+            return exception;
+        }
+    }
+}
---
+++ src/darknamer-functional/Utility/UrlUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Utility
+{
+    public class UrlUtility
+    {
+        public static string QueryString(object jsonBody)
+        {
+            var json = JsonConvert.SerializeObject(jsonBody);
+            var jObj = (JObject)JsonConvert.DeserializeObject(json);
+            var queryString = string.Join("&",
+                jObj.Children().Cast<JProperty>()
+                    .Select(jp => jp.Name + "=" + System.Web.HttpUtility.UrlEncode(jp.Value.ToString())));
+            // -- Debug.WriteLine("UrlUtil.QueryString(jsonBody = default(null)) = {0}", queryString); --
+            return queryString;
+        }
+    }
+}
